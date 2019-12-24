fb7e6421988209a82d612c4ae2887fce51fb1f6a
A. Zain <57663136+gbadnanzain@users.noreply.github.com>
2019-12-24 11:10:39 +0700

    commit on master

--- lib/model.dart
+++ lib/model.dart
@@ -3,6 +3,53 @@ import 'package:http/http.dart' as http;
 import 'dart:async';
 import 'dart:convert';
 
-class IPModel{
 
+class Category {
+  String idCategory;
+  String strCategory;
+  String strCategoryThumb;
+  Category(
+    {this.idCategory,
+    this.strCategory,
+    this.strCategoryThumb
+  });
+
+  factory Category.fromJson(Map<string, dynamic> parsedJson) {
+    
+  }
+
+}
+
+
+class Meal {
+  String idMeal;
+  String strMeal;
+  String strInstructions;
+  String strMealThumb;
+  String strIngredient;
+
+  Meal(
+      {this.idMeal,
+      this.strMeal,
+      this.strInstructions,
+      this.strMealThumb,
+      this.strIngredient});
+
+  factory Meal.fromJson(Map<String, dynamic> parsedJson) {
+    String _strIngredient = parsedJson['strIngredient1'] +
+        ", " +
+        parsedJson['strIngredient2'] +
+        ", " +
+        parsedJson['strIngredient3'] +
+        ", " +
+        parsedJson['strIngredient4'] +
+        ", " +
+        parsedJson['strIngredient5'];
+    return Meal(
+        idMeal: parsedJson['idMeal'],
+        strMeal: parsedJson['strMeal'],
+        strInstructions: parsedJson['strInstructions'],
+        strMealThumb: parsedJson['strMealThumb'],
+        strIngredient: _strIngredient);
+  }
 }
